6f0e367ba8f583c17909c8aac8121eca7e93abf7
sunyuting83 <sleepsun123@163.com>
2019-12-05 21:18:44 +0800

    sdfsdf

--- scropy/httpUtils.js
+++ scropy/httpUtils.js
@@ -1,38 +1,31 @@
 var superagent = require('superagent');
-<<<<<<< HEAD
-var sleep = require('system-sleep');
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'; // 解决https无法访问的问题
-let HttpGet = async (url) => {
-    await sleep(200);
-=======
 // var sleep = require('system-sleep');
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'; // 解决https无法访问的问题
 let HttpGet = async (url) => {
-    // await sleep(200);
->>>>>>> master
-    var ip = Math.floor((Math.random()*254)+1)
-    + "." + Math.floor((Math.random()*254)+1)
-    + "." + Math.floor((Math.random()*254)+1)
-    + "." + Math.floor((Math.random()*254)+1);
-    // console.log(ip);
-    return new Promise(async(resolve, reject) => {
-        // console.log(url);
-        if (url !== NaN || url !== undefined) {
-            superagent.get(url)
-                .set("X-Forwarded-For" , ip)
-                .set({
-                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3100.0 Safari/537.36",
-                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
-                })
-                .then((res) => {
-                    resolve(res.text)
-                })
-                .catch((e) => {
-                    console.log(e.message);
-                    process.exit();
-                });
-        }
-    });
+  // await sleep(200);
+  var ip = Math.floor((Math.random() * 254) + 1) +
+    "." + Math.floor((Math.random() * 254) + 1) +
+    "." + Math.floor((Math.random() * 254) + 1) +
+    "." + Math.floor((Math.random() * 254) + 1);
+  // console.log(ip);
+  return new Promise(async (resolve, reject) => {
+    // console.log(url);
+    if (url !== NaN || url !== undefined) {
+      superagent.get(url)
+        .set("X-Forwarded-For", ip)
+        .set({
+          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3100.0 Safari/537.36",
+          'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+        })
+        .then((res) => {
+          resolve(res.text)
+        })
+        .catch((e) => {
+          console.log(e.message);
+          process.exit();
+        });
+    }
+  });
 };
 
 module.exports = HttpGet;
